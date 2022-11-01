21308d5b51fcc72740a5cf7872c4941ff775bb8f
Evan Thebearge <62355703+evanthebearge@users.noreply.github.com>
2022-11-01 11:14:43 -0400

    Update charge.ino
    includes comments

--- charge.ino
+++ charge.ino
@@ -1,6 +1,6 @@
-void setup(){
-pinMode P0_13, OUTPUT);
+void setup() {
+  pinMode P0_13, OUTPUT); // Initiate battery charging
 }
 void loop() {
-digitalWrite(P0_13, LOW);
+  digitalWrite(P0_13, LOW); // 100mA battery charging current
 }
