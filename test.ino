d722e277346bc1ce20ccc8a99552a738eca94b7e
Evan Thebearge <62355703+evanthebearge@users.noreply.github.com>
2022-11-02 09:25:59 -0400

    Update test.ino

--- test.ino
+++ test.ino
@@ -1,47 +1,70 @@
-/*
-  Blink
-
-  Turns an LED on for one second, then off for one second, repeatedly.
-
-  Most Arduinos have an on-board LED you can control. On the UNO, MEGA and ZERO
-  it is attached to digital pin 13, on MKR1000 on pin 6. LED_BUILTIN is set to
-  the correct LED pin independent of which board is used.
-  If you want to know what pin the on-board LED is connected to on your Arduino
-  model, check the Technical Specs of your board at:
-  https://www.arduino.cc/en/Main/Products
-
-  modified 8 May 2014
-  by Scott Fitzgerald
-  modified 2 Sep 2016
-  by Arturo Guadalupi
-  modified 8 Sep 2016
-  by Colby Newman
-
-  This example code is in the public domain.
-
-  https://www.arduino.cc/en/Tutorial/BuiltInExamples/Blink
-*/
-
-// the setup function runs once when you press reset or power the board
+#include <ArduinoBLE.h>
+ 
+BLEService ledService("19B10000-E8F2-537E-4F6C-D104768A1214"); // Bluetooth® Low Energy LED Service
+ 
+// Bluetooth® Low Energy LED Switch Characteristic - custom 128-bit UUID, read and writable by central
+BLEByteCharacteristic switchCharacteristic("19B10001-E8F2-537E-4F6C-D104768A1214", BLERead | BLEWrite);
+ 
+const int ledPin = LED_BUILTIN; // pin to use for the LED
+ 
 void setup() {
-  // initialize digital pin LED_BUILTIN as an output.
-  pinMode(LED_BUILTIN, OUTPUT);
-  pinMode(3, OUTPUT);
-  pinMode(4, OUTPUT);
+  Serial.begin(9600);
+  while (!Serial);
+ 
+  // set LED pin to output mode
+  pinMode(ledPin, OUTPUT);
+ 
+  // begin initialization
+  if (!BLE.begin()) {
+    Serial.println("starting Bluetooth® Low Energy module failed!");
+ 
+    while (1);
+  }
+ 
+  // set advertised local name and service UUID:
+  BLE.setLocalName("LED");
+  BLE.setAdvertisedService(ledService);
+ 
+  // add the characteristic to the service
+  ledService.addCharacteristic(switchCharacteristic);
+ 
+  // add service
+  BLE.addService(ledService);
+ 
+  // set the initial value for the characeristic:
+  switchCharacteristic.writeValue(0);
+ 
+  // start advertising
+  BLE.advertise();
+ 
+  Serial.println("BLE LED Peripheral");
 }
-
-// the loop function runs over and over again forever
+ 
 void loop() {
-  digitalWrite(LED_BUILTIN, HIGH);   // turn the LED on (HIGH is the voltage level)
-  delay(1000);                       // wait for a second
-  digitalWrite(LED_BUILTIN, LOW);    // turn the LED off by making the voltage LOW
-  delay(1000);                       // wait for a second
-  digitalWrite(3, HIGH);
-  delay(1000);
-  digitalWrite(3, LOW);
-  delay(1000);
-  digitalWrite(4, HIGH);
-  delay(1000);
-  digitalWrite(4, LOW);
-  
+  // listen for Bluetooth® Low Energy peripherals to connect:
+  BLEDevice central = BLE.central();
+ 
+  // if a central is connected to peripheral:
+  if (central) {
+    Serial.print("Connected to central: ");
+    // print the central's MAC address:
+    Serial.println(central.address());
+ 
+    // while the central is still connected to peripheral:
+  while (central.connected()) {
+        if (switchCharacteristic.written()) {
+          if (switchCharacteristic.value()) {   
+            Serial.println("LED on");
+            digitalWrite(ledPin, LOW); // changed from HIGH to LOW       
+          } else {                              
+            Serial.println(F("LED off"));
+            digitalWrite(ledPin, HIGH); // changed from LOW to HIGH     
+          }
+        }
+      }
+ 
+    // when the central disconnects, print it out:
+    Serial.print(F("Disconnected from central: "));
+    Serial.println(central.address());
+  }
 }
