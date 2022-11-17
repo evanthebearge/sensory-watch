1380d7df1115b6fdf3ffd9695824bb420217b08e
Evan Thebearge <62355703+evanthebearge@users.noreply.github.com>
2022-11-17 12:43:01 -0500

    Update temp.ino

--- temp.ino
+++ temp.ino
@@ -1,1 +1,20 @@
+/*
+POSSIBLY REMOVE?
+// set advertised services
+  BLE.setAdvertisedService("ledServiceWATCH0");
+  BLE.setAdvertisedService("motorServiceWATCH0");
+  BLE.setAdvertisedService("button0ServiceWATCH0");
+  BLE.setAdvertisedService("button1ServiceWATCH0");
+    
+// add the characteristics to the services
+  ledServiceWATCH0.addCharacteristic("ledCharacteristicWATCH0");
+  motorServiceWATCH0.addCharacteristic("motorCharacteristicWATCH0");
+  button0ServiceWATCH0.addCharacteristic("button0CharacteristicWATCH0");
+  button1ServiceWATCH0.addCharacteristic("button1CharacteristicWATCH0");
 
+// add services
+  BLE.addService(ledServiceWATCH0);
+  BLE.addService(motorServiceWATCH0);
+  BLE.addService(button0ServiceWATCH0);
+  BLE.addService(button1ServiceWATCH0);
+*/
